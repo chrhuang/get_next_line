b182a24d989a7d28528e448d205d2331fcbca4f7
Christian HUANG <chrhuang@e2r2p2.42.fr>
2018-12-05 08:04:49 +0100

    sdf

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: chrhuang <chrhuang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 14:07:06 by chrhuang          #+#    #+#             */
-/*   Updated: 2018/12/03 11:36:30 by chrhuang         ###   ########.fr       */
+/*   Updated: 2018/12/04 15:09:15 by chrhuang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 10000
+# define BUFF_SIZE 42
 
 int		get_next_line(const int fd, char **line);
 
